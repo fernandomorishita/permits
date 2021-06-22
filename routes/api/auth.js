4ec71680e9ca06e16cb56a31479c2cffbdd7a02b
Hevnie <fy.morishita@gmail.com>
2021-06-21 22:14:23 -0300

    prepare deploy

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -56,7 +56,7 @@ router.post('/', [check('email', 'Include a valid email').isEmail(), check('pass
       }
     }
 
-    jwt.sign(payload, config.get('jwtSecret'), { expiresIn: 360000 }, (err, token) => {
+    jwt.sign(payload, /*config.get('jwtSecret')*/ process.env.JWT_SECRET, { expiresIn: 360000 }, (err, token) => {
       if (err) throw err
       res.json({ token })
     })
--- server.js
+++ server.js
@@ -1,14 +1,13 @@
 const express = require('express')
 const connectDB = require('./config/db')
 const app = express()
+const path = require('path')
 
 // Connect Database
 connectDB()
 
 // Init Middleware
 app.use(express.json({ extended: false }))
-
-app.get('/', (req, res) => res.send('API Running'))
 
 // Define Routes
 app.use('/api/user', require('./routes/api/user'))
@@ -19,6 +18,16 @@ app.use('/api/queries', require('./routes/api/queries'))
 app.use('/api/visaoffices', require('./routes/api/visaoffices'))
 app.use('/api/vacs', require('./routes/api/vacs'))
 app.use('/api/credentials', require('./routes/api/credentials'))
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'))
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
+}
 
 const PORT = process.env.PORT || 5000
 
